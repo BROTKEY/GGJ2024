2dbb54b9b5716c69f6dfae4b166a1b6a3456afcc
Simon Pirkelmann <simon.pirkelmann@gmail.com>
2024-01-27 14:27:26 +0100

    refactored code into multiple files

---
+++ ggj2024/config.py
@@ -0,0 +1,64 @@
+SCREEN_TITLE = "PyMunk Platformer"
+
+# How big are our image tiles?
+SPRITE_IMAGE_SIZE = 128
+
+# Scale sprites up or down
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_TILES = 0.5
+
+# Scaled sprite size for tiles
+SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_PLAYER)
+
+# Size of grid to show on screen, in number of tiles
+SCREEN_GRID_WIDTH = 10
+SCREEN_GRID_HEIGHT = 10
+
+# Size of screen to show, in pixels
+SCREEN_WIDTH = SPRITE_SIZE * SCREEN_GRID_WIDTH
+SCREEN_HEIGHT = SPRITE_SIZE * SCREEN_GRID_HEIGHT
+
+# --- Physics forces. Higher number, faster accelerating.
+
+# Gravity
+GRAVITY = 2500
+
+# Damping - Amount of speed lost per second
+DEFAULT_DAMPING = 1.0
+PLAYER_DAMPING = 0.4
+
+# Friction between objects
+PLAYER_FRICTION = 1.0
+WALL_FRICTION = 0.7
+DYNAMIC_ITEM_FRICTION = 0.6
+
+# Mass (defaults to 1)
+PLAYER_MASS = 2.0
+
+# Keep player from going too fast
+PLAYER_MAX_HORIZONTAL_SPEED = 10000
+PLAYER_MAX_VERTICAL_SPEED = 10000
+
+# Force applied while on the ground
+PLAYER_MOVE_FORCE_ON_GROUND = 8000
+# Force applied when moving left/right in the air
+PLAYER_MOVE_FORCE_IN_AIR = 5000
+
+# Strength of a jump
+PLAYER_JUMP_IMPULSE = 1500
+
+PLAYER_DEATH_IMPULSE = 2000
+
+# Close enough to not-moving to have the animation go to idle.
+DEAD_ZONE = 0.1
+
+# Constants used to track if the player is facing left or right
+RIGHT_FACING = 0
+LEFT_FACING = 1
+
+# How many pixels to move before we change the texture in the walking animation
+DISTANCE_TO_CHANGE_TEXTURE = 20
+
+# Defines whether forces on the player are considered to be in the Player's coordinate system
+# This switch is basically here to keep some of the old code...
+FORCES_RELATIVE_TO_PLAYER = True
---
+++ ggj2024/sprites.py
@@ -0,0 +1,106 @@
+import arcade
+
+from ggj2024.config import *
+
+
+class PlayerSprite(arcade.Sprite):
+    """ Player Sprite """
+    def __init__(self,
+                #  ladder_list: arcade.SpriteList,
+                 hit_box_algorithm):
+        """ Init """
+        # Let parent initialize
+        super().__init__()
+
+        # Set our scale
+        self.scale = SPRITE_SCALING_PLAYER
+
+        # Images from Kenney.nl's Character pack
+        # main_path = ":resources:images/animated_characters/female_adventurer/femaleAdventurer"
+        main_path = ":resources:images/animated_characters/female_person/femalePerson"
+        # main_path = ":resources:images/animated_characters/male_person/malePerson"
+        # main_path = ":resources:images/animated_characters/male_adventurer/maleAdventurer"
+        # main_path = ":resources:images/animated_characters/zombie/zombie"
+        # main_path = ":resources:images/animated_characters/robot/robot"
+
+        # Load textures for idle standing
+        self.idle_texture_pair = arcade.load_texture_pair(f"{main_path}_idle.png",
+                                                          hit_box_algorithm=hit_box_algorithm)
+        self.jump_texture_pair = arcade.load_texture_pair(f"{main_path}_jump.png")
+        self.fall_texture_pair = arcade.load_texture_pair(f"{main_path}_fall.png")
+
+        # Load textures for walking
+        self.walk_textures = []
+        for i in range(8):
+            texture = arcade.load_texture_pair(f"{main_path}_walk{i}.png")
+            self.walk_textures.append(texture)
+
+        # Set the initial texture
+        self.texture = self.idle_texture_pair[0]
+
+        # Hit box will be set based on the first image used.
+        self.hit_box = self.texture.hit_box_points
+
+        # Default to face-right
+        self.character_face_direction = RIGHT_FACING
+
+        # Index of our current texture
+        self.cur_texture = 0
+
+        # How far have we traveled horizontally since changing the texture
+        self.x_odometer = 0
+        self.y_odometer = 0
+
+    def pymunk_moved(self, physics_engine, dx, dy, d_angle):
+        """ Handle being moved by the pymunk engine """
+        # Figure out if we need to face left or right
+        if dx < -DEAD_ZONE and self.character_face_direction == RIGHT_FACING:
+            self.character_face_direction = LEFT_FACING
+        elif dx > DEAD_ZONE and self.character_face_direction == LEFT_FACING:
+            self.character_face_direction = RIGHT_FACING
+
+        # Are we on the ground?
+        is_on_ground = physics_engine.is_on_ground(self)
+
+        # Add to the odometer how far we've moved
+        self.x_odometer += dx
+        self.y_odometer += dy
+
+        # Jumping animation
+        if not is_on_ground:
+            if dy > DEAD_ZONE:
+                self.texture = self.jump_texture_pair[self.character_face_direction]
+                return
+            elif dy < -DEAD_ZONE:
+                self.texture = self.fall_texture_pair[self.character_face_direction]
+                return
+
+        # Idle animation
+        if abs(dx) <= DEAD_ZONE:
+            self.texture = self.idle_texture_pair[self.character_face_direction]
+            return
+
+        # Have we moved far enough to change the texture?
+        if abs(self.x_odometer) > DISTANCE_TO_CHANGE_TEXTURE:
+
+            # Reset the odometer
+            self.x_odometer = 0
+
+            # Advance the walking animation
+            self.cur_texture += 1
+            if self.cur_texture > 7:
+                self.cur_texture = 0
+            self.texture = self.walk_textures[self.cur_texture][self.character_face_direction]
+
+
+class PhysicsSprite(arcade.Sprite):
+    def __init__(self, pymunk_shape, filename):
+        super().__init__(filename, center_x=pymunk_shape.body.position.x, center_y=pymunk_shape.body.position.y)
+        self.pymunk_shape = pymunk_shape
+
+
+class ControllablePlatformSprite(PhysicsSprite):
+    def __init__(self, pymunk_shape, filename, width, height):
+        super().__init__(pymunk_shape, filename)
+        self.width = width
+        self.height = height
