60b21eb409f5fd66d0722c1fa14b10be8175d200
BROTKEY <54113864+BROTKEY@users.noreply.github.com>
2024-01-27 22:05:38 +0100

    slice tree

--- assets/General.tsx
+++ assets/General.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="General" tilewidth="256" tileheight="512" tilecount="12" columns="0">
+<tileset version="1.8" tiledversion="1.8.0" name="General" tilewidth="256" tileheight="256" tilecount="19" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="20">
   <image width="128" height="128" source="TILES/fence.png"/>
@@ -36,15 +36,31 @@
    </object>
   </objectgroup>
  </tile>
- <tile id="36">
-  <image width="256" height="512" source="TILES/tree.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="5" x="27.9548" y="161.02">
-    <polygon points="0,0 17.8911,-10.0637 30.1912,-1.11819 48.0823,-25.7184 63.737,-32.4276 72.6825,-24.6002 98.401,-55.9097 111.819,-58.146 115.174,-45.8459 133.065,-49.2005 150.956,-41.3731 158.783,-23.4821 155.429,17.8911 168.847,19.0093 178.911,38.0186 180.029,62.6188 164.374,95.0464 171.084,103.992 166.611,120.765 147.601,133.065 137.538,145.365 111.819,161.02 95.0464,201.275 88.3372,244.884 90.5736,258.303 98.401,246.002 101.756,252.712 103.992,260.539 96.1646,260.539 91.6918,263.894 116.292,345.522 230.348,344.403 230.348,352.231 -29.073,353.349 -25.7184,341.049 42.4913,342.167 54.7915,282.903 49.2005,224.757 34.664,169.965 7.82735,142.011 -24.6002,126.356 -21.2457,108.465 -25.7184,93.9282 -23.4821,62.6188 -15.6547,21.2457"/>
-   </object>
-  </objectgroup>
- </tile>
  <tile id="37">
   <image width="256" height="256" source="TILES/cloud.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="TILES/tree_1.png"/>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="TILES/tree_2.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="TILES/tree_3.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="TILES/tree_4.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="TILES/tree_5.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="TILES/tree_6.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="TILES/tree_7.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="TILES/tree_8.png"/>
  </tile>
 </tileset>
