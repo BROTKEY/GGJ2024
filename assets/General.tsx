97c35fc738ad67f50c0e193b9af46f9cb63c210e
BROTKEY <54113864+BROTKEY@users.noreply.github.com>
2024-01-27 21:07:29 +0100

    WIP

--- assets/General.tsx
+++ assets/General.tsx
@@ -1,57 +1,37 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="General" tilewidth="128" tileheight="128" tilecount="9" columns="0">
+<tileset version="1.8" tiledversion="1.8.0" name="General" tilewidth="256" tileheight="512" tilecount="11" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="7">
-  <image width="128" height="128" source="TILES/Bush.png"/>
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="45.2377" y="127.251">
-    <polygon points="0,0 2.92906,-0.325451 1.30181,-0.976354"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="8">
+ <tile id="20">
   <image width="128" height="128" source="TILES/fence.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="40.0305" y="126.926">
-    <polygon points="0,0 2.92906,0.325451 0.650903,-1.30181"/>
-   </object>
-  </objectgroup>
  </tile>
- <tile id="9">
+ <tile id="21">
   <image width="128" height="128" source="TILES/Flag.png"/>
  </tile>
- <tile id="10">
-  <image width="128" height="128" source="TILES/Flat.png"/>
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="0.325451" y="-0.325451" width="127.251" height="127.902"/>
-  </objectgroup>
- </tile>
- <tile id="11">
-  <image width="128" height="128" source="TILES/Flat+root.png"/>
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="0" y="-0.325451" width="127.577" height="128.553"/>
-  </objectgroup>
- </tile>
- <tile id="13">
-  <image width="128" height="128" source="TILES/Flower2.png"/>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="52.7231" y="127.577">
-    <polygon points="0,0 1.30181,0 0.650903,-0.976354"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="14">
+ <tile id="26">
   <image width="128" height="128" source="TILES/Platform.png"/>
  </tile>
- <tile id="15">
+ <tile id="27">
   <image width="128" height="128" source="TILES/Slope.png"/>
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="0" y="0.325451">
-    <polygon points="0,0 128.553,127.902 0,127.577"/>
-   </object>
-  </objectgroup>
  </tile>
- <tile id="17">
+ <tile id="28">
+  <image width="256" height="512" source="TILES/tree.png"/>
+ </tile>
+ <tile id="30">
   <image width="128" height="128" source="TILES/White.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="TILES/Flat.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="TILES/Flat+root.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="TILES/Flower2.png"/>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="TILES/Bush.png"/>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="TILES/grass.png"/>
  </tile>
 </tileset>
